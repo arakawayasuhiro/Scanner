66a47a6c2e431c59fbc2361ec5be007d65fe50c8
yasuhiro <yasuhiro.arakawa@gmail.com>
2026-04-17 09:40:21 +0900

    cliping of drawing detected text rectangles

--- app/src/main/java/com/example/scanner/ui/contents/PreviewContents.kt
+++ app/src/main/java/com/example/scanner/ui/contents/PreviewContents.kt
@@ -17,9 +17,11 @@ import androidx.compose.runtime.produceState
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Matrix
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.drawscope.Fill
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.setFrom
@@ -83,7 +85,11 @@ fun PreviewContents(
             }
         }
 
-        Canvas(Modifier.fillMaxSize()) {
+        Canvas(
+            Modifier
+                .fillMaxSize()
+                .clip(RectangleShape)
+        ) {
             poi?.let {
                 val radius = 40f
                 drawCircle(
